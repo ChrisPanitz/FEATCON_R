2644ec7389eccb31b2a7baa553f73616fa9215e4
Christian Panitz <panitzc@gmail.com>
2025-05-20 01:29:30 +0200

    more plottting and playing around in example script

--- computationalModeling/exampleScriptKalman.R
+++ computationalModeling/exampleScriptKalman.R
@@ -10,11 +10,11 @@ source(paste0(parentFolder, "/computationalModeling/customFunctions/kalmanFilter
 source(paste0(parentFolder, "/computationalModeling/customFunctions/optimizeParametersKalman.R"))
 
 
-loadfile = paste0(parentFolder,"/computationalModeling/logfiles/103_FEATCON_logfile.txt")
+loadfile = paste0(parentFolder,"/computationalModeling/logfiles/105_FEATCON_logfile.txt")
 partData <- importFeatconLog(loadfile)
 partDataNoHab <- partData[partData$trial>0,]
 
-stimArrays <- createStimArrays(partData, csCoding = "combo")
+stimArrays <- createStimArrays(partData, csCoding = "feature")
 
 optiParam <- optimizeParametersKalman(empData = partDataNoHab$contRating,
                                       csArray = stimArrays$csArray,
@@ -22,15 +22,36 @@ optiParam <- optimizeParametersKalman(empData = partDataNoHab$contRating,
                                       startW = 0,
                                       expectationTime = "post",
                                       tauSqFixed = FALSE, sigmaSqRFixed = FALSE,
-                                      tauSq = 0.01, sigmaSqR = 0.20) # start values for optimization
+                                      tauSq = 0.01, sigmaSqR = 0.25) # start values for optimization
+
+optiParam <- optimizeParametersKalman(empData = partDataNoHab$contRating[partDataNoHab$cs1 == 4],
+                                      csArray = stimArrays$csArray[,partDataNoHab$cs1 == 4],
+                                      usArray = stimArrays$usArray[partDataNoHab$cs1 == 4],
+                                      startW = 0,
+                                      expectationTime = "post",
+                                      tauSqFixed = FALSE, sigmaSqRFixed = FALSE,
+                                      tauSq = 0.01, sigmaSqR = 0.25) # start values for optimization
   
 optiParam$par
 
 optiKalman <- kalmanFilter(csArray = stimArrays$csArray,
                            usArray = stimArrays$usArray,
                            startW = 0,
+                           #tauSq = 0.001, sigmaSqR = 0.20)
                            tauSq = optiParam$par["tauSq"],
                            sigmaSqR = optiParam$par["sigmaSqR"])
+
+###
+# ggplot() +
+#   geom_line(aes(x = 0:160, y = optiKalman$assocStrength[1,]), color = "red") +
+#   geom_line(aes(x = 0:160, y = optiKalman$assocStrength[2,]), color = "green") +
+#   geom_line(aes(x = 0:160, y = optiKalman$assocStrength[3,])) +
+#   geom_point(aes(x = 0:160, y = optiKalman$assocStrength[3,]), shape = "circle") +
+#   geom_line(aes(x = 0:160, y = optiKalman$assocStrength[4,])) +
+#   geom_point(aes(x = 0:160, y = optiKalman$assocStrength[4,]), shape = "diamond")
+
+###
+
 
 rmse <- sqrt(mean((partDataNoHab$contRating - optiKalman$outcomeExpPost)^2))
 corr <- cor(partDataNoHab$contRating, optiKalman$outcomeExpPost)
@@ -50,45 +71,70 @@ corr_mm <- cor(partDataNoHab$contRating[partDataNoHab$cs1 == 4], optiKalman$outc
 
 # plot all trials
 plotCSpp <- ggplot() +
-  geom_line(aes(x = 1:40, y = scale(partDataNoHab$contRating[partDataNoHab$cs1==1])), color = "black") +
-  geom_line(aes(x = 1:40, y = scale(optiKalman$outcomeExpPost[partDataNoHab$cs1==1])), color = "blue") +
-  #geom_vline(xintercept = which(partDataNoHab$us[partDataNoHab$cs1==1] == 1), color = "gray") +
-  geom_point(aes(x = which(partDataNoHab$us[partDataNoHab$cs1==1] == 1), y = 1.5)) +
-  geom_label(aes(x = 40, y = 1.3, label = paste0("r = ", as.character(round(corr_pp,2)))), color = "black") +
+  geom_line(aes(x = 1:40, y = partDataNoHab$contRating[partDataNoHab$cs1==1]), color = "black") +
+  geom_line(aes(x = 1:40, y = optiKalman$outcomeExpPost[partDataNoHab$cs1==1]*100), color = "blue") +
+  geom_point(aes(x = which(partDataNoHab$us[partDataNoHab$cs1==1] == 1), y = 120)) +
+  geom_label(aes(x = 10, y = 5, label = paste0("r = ", as.character(round(corr_pp,2)))), color = "black") +
+  geom_label(aes(x = 20, y = 5, label = paste0("tau^2 = ", as.character(round(optiKalman$tauSq,2)))), color = "black") +
+  geom_label(aes(x = 30, y = 5, label = paste0("sigma^2R = ", as.character(round(optiKalman$sigmaSqR,2)))), color = "black") +
   scale_x_continuous(name = "trial #") +
-  scale_y_continuous(name = "z-values of predicted (blue) and observed (black) responses") +
-  theme_classic()
+  scale_y_continuous(name = "Probability Rating (0-100%)", breaks = seq(0,100,20),
+                     sec.axis = sec_axis(name = "Kalman Prediction (blue)", trans = ~./100, breaks = seq(0.0, 1.0, 0.2))) +
+  theme_classic() +
+  theme(axis.line.y.right = element_line(color = "blue"),
+        axis.ticks.y.right = element_line(color = "blue"),
+        axis.text.y.right = element_text(color = "blue"),
+        axis.title.y.right = element_text(color = "blue"))
 plotCSpp
 
 plotCSpm <- ggplot() +
-  geom_line(aes(x = 1:40, y = scale(partDataNoHab$contRating[partDataNoHab$cs1==2])), color = "black") +
-  geom_line(aes(x = 1:40, y = scale(optiKalman$outcomeExpPost[partDataNoHab$cs1==2])), color = "blue") +
-  #geom_vline(xintercept = which(partDataNoHab$us[partDataNoHab$cs1==1] == 1), color = "gray") +
-  geom_point(aes(x = which(partDataNoHab$us[partDataNoHab$cs1==1] == 1), y = 1.5)) +
-  geom_label(aes(x = 40, y = 1.3, label = paste0("r = ", as.character(round(corr_pm,2)))), color = "black") +
+  geom_line(aes(x = 1:40, y = partDataNoHab$contRating[partDataNoHab$cs1==2]), color = "black") +
+  geom_line(aes(x = 1:40, y = optiKalman$outcomeExpPost[partDataNoHab$cs1==2]*100), color = "blue") +
+  geom_point(aes(x = which(partDataNoHab$us[partDataNoHab$cs1==2] == 1), y = 120)) +
+  geom_label(aes(x = 10, y = 5, label = paste0("r = ", as.character(round(corr_pm,2)))), color = "black") +
+  geom_label(aes(x = 20, y = 5, label = paste0("tau^2 = ", as.character(round(optiKalman$tauSq,2)))), color = "black") +
+  geom_label(aes(x = 30, y = 5, label = paste0("sigma^2R = ", as.character(round(optiKalman$sigmaSqR,2)))), color = "black") +
   scale_x_continuous(name = "trial #") +
-  scale_y_continuous(name = "z-values of predicted (blue) and observed (black) responses") +
-  theme_classic()
+  scale_y_continuous(name = "Probability Rating (0-100%)", breaks = seq(0,100,20),
+                     sec.axis = sec_axis(name = "Kalman Prediction (blue)", trans = ~./100, breaks = seq(0.0, 1.0, 0.2))) +
+  theme_classic() +
+  theme(axis.line.y.right = element_line(color = "blue"),
+        axis.ticks.y.right = element_line(color = "blue"),
+        axis.text.y.right = element_text(color = "blue"),
+        axis.title.y.right = element_text(color = "blue"))
 plotCSpm
 
 plotCSmp <- ggplot() +
-  geom_line(aes(x = 1:40, y = scale(partDataNoHab$contRating[partDataNoHab$cs1==3])), color = "black") +
-  geom_line(aes(x = 1:40, y = scale(optiKalman$outcomeExpPost[partDataNoHab$cs1==3])), color = "blue") +
-  #geom_vline(xintercept = which(partDataNoHab$us[partDataNoHab$cs1==1] == 1), color = "gray") +
-  geom_point(aes(x = which(partDataNoHab$us[partDataNoHab$cs1==3] == 1), y = 1.5)) +
-  geom_label(aes(x = 40, y = 1.3, label = paste0("r = ", as.character(round(corr_mp,2)))), color = "black") +
+  geom_line(aes(x = 1:40, y = partDataNoHab$contRating[partDataNoHab$cs1==3]), color = "black") +
+  geom_line(aes(x = 1:40, y = optiKalman$outcomeExpPost[partDataNoHab$cs1==3]*100), color = "blue") +
+  geom_point(aes(x = which(partDataNoHab$us[partDataNoHab$cs1==3] == 1), y = 120)) +
+  geom_label(aes(x = 10, y = 5, label = paste0("r = ", as.character(round(corr_mp,2)))), color = "black") +
+  geom_label(aes(x = 20, y = 5, label = paste0("tau^2 = ", as.character(round(optiKalman$tauSq,2)))), color = "black") +
+  geom_label(aes(x = 30, y = 5, label = paste0("sigma^2R = ", as.character(round(optiKalman$sigmaSqR,2)))), color = "black") +
   scale_x_continuous(name = "trial #") +
-  scale_y_continuous(name = "z-values of predicted (blue) and observed (black) responses") +
-  theme_classic()
+  scale_y_continuous(name = "Probability Rating (0-100%)", breaks = seq(0,100,20),
+                     sec.axis = sec_axis(name = "Kalman Prediction (blue)", trans = ~./100, breaks = seq(0.0, 1.0, 0.2))) +
+  theme_classic() +
+  theme(axis.line.y.right = element_line(color = "blue"),
+        axis.ticks.y.right = element_line(color = "blue"),
+        axis.text.y.right = element_text(color = "blue"),
+        axis.title.y.right = element_text(color = "blue"))
 plotCSmp
 
 plotCSmm <- ggplot() +
-  geom_line(aes(x = 1:40, y = scale(partDataNoHab$contRating[partDataNoHab$cs1==4])), color = "black") +
-  geom_line(aes(x = 1:40, y = scale(optiKalman$outcomeExpPost[partDataNoHab$cs1==4])), color = "blue") +
-  #geom_vline(xintercept = which(partDataNoHab$us[partDataNoHab$cs1==1] == 1), color = "gray") +
-  geom_point(aes(x = which(partDataNoHab$us[partDataNoHab$cs1==4] == 1), y = 1.5)) +
-  geom_label(aes(x = 40, y = 1.3, label = paste0("r = ", as.character(round(corr_mm,2)))), color = "black") +
+  geom_line(aes(x = 1:40, y = partDataNoHab$contRating[partDataNoHab$cs1==4]), color = "black") +
+  geom_line(aes(x = 1:40, y = optiKalman$outcomeExpPost[partDataNoHab$cs1==4]*100), color = "blue") +
+  geom_point(aes(x = which(partDataNoHab$us[partDataNoHab$cs1==4] == 1), y = 120)) +
+  geom_label(aes(x = 10, y = 5, label = paste0("r = ", as.character(round(corr_mm,2)))), color = "black") +
+  geom_label(aes(x = 20, y = 5, label = paste0("tau^2 = ", as.character(round(optiKalman$tauSq,2)))), color = "black") +
+  geom_label(aes(x = 30, y = 5, label = paste0("sigma^2R = ", as.character(round(optiKalman$sigmaSqR,2)))), color = "black") +
   scale_x_continuous(name = "trial #") +
-  scale_y_continuous(name = "z-values of predicted (blue) and observed (black) responses") +
-  theme_classic()
+  scale_y_continuous(name = "Probability Rating (0-100%)", breaks = seq(0,100,20),
+                     sec.axis = sec_axis(name = "Kalman Prediction (blue)", trans = ~./100, breaks = seq(0.0, 1.0, 0.2))) +
+  theme_classic() +
+  theme(axis.line.y.right = element_line(color = "blue"),
+        axis.ticks.y.right = element_line(color = "blue"),
+        axis.text.y.right = element_text(color = "blue"),
+        axis.title.y.right = element_text(color = "blue"))
 plotCSmm
+
